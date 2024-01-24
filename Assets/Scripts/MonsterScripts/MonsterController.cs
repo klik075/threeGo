18bf2959c7bb9537a30663e7557a479779e45978
정원우 <54103715+klik075@users.noreply.github.com>
2024-01-24 17:55:14 +0900

    feat - girl 애니메이션, 컨트롤러 생성
    걸 run , dead 애니메이션 추가
    몬스터 컨트롤러 생성

---
+++ Assets/Scripts/MonsterScripts/MonsterController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : CharacterStat
+{
+    protected Transform ClosetTarget {get; private set;}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
